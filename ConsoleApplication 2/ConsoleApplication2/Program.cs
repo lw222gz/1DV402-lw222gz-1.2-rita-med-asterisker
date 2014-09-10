5a0ad86beb99b01d4cb9e645605074aecd3220d2
Lucas <lw222gz@NY113X08.lnu.se>
2014-09-10 17:36:13 +0200

    I hope this works. Finished labb 1.1 aswell as 1.2

---
+++ ConsoleApplication 2/ConsoleApplication2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int värde = 0;
+            
+            for (int i = 0; i < 25; i++)
+            {
+
+                               
+                if (värde > 2)
+                {
+                    värde = 0;
+                }
+                
+                switch (värde)
+                {
+                    case 0:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        break;
+                    case 1:
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        break;
+                    case 2:                        
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        break;                   
+                }
+                ++värde;
+                
+                
+                for (int j = 0; j < 39; j++)
+                {                   
+                    Console.Write("* ");
+                }
+                
+                Console.WriteLine();
+                
+
+                if (i % 2 == 0)
+                {
+                    Console.Write(" ");
+                }
+                
+             
+        
+             }
+            Console.ResetColor();
+                
+        
+             
+            
+
+        }
+    }
+}
